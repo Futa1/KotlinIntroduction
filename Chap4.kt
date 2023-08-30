5cf8f7f26e9f836194d6294f4d33caa89b96fcf5
Futa1 <futa1997105@gmail.com>
2023-08-30 21:48:03 +0900

    chapter 4 getTriangleArea function implement

--- Chap4.kt
+++ Chap4.kt
@@ -1,2 +1,21 @@
 package com.example.kotlinpractice
 
+fun main() {
+    println(getTriangleArea(5.0, 10.0))
+
+}
+
+/**
+ * 三角形の面積を求める
+ *
+ * @param[width] 三角形の底辺の長さ
+ * @param[height] 三角形の高さ
+ * @return 面積の値
+ */
+fun getTriangleArea(width: Double, height: Double): Double {
+    return width * height / 2
+}
+// 関数の本体が1行のみなら次のような省略も可能
+// fun getTriangleArea(width: Double, height: Double): Double = width * height / 2
+// fun getTriangleArea(width: Double. height: Double) = width * height / 2
+
