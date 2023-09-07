aa6ef2d2c776491a9851381ea026f4eafa82f27b
Futa1 <futa1997105@gmail.com>
2023-09-07 20:41:31 +0900

    chapter5 finished

--- Chap5.kt
+++ Chap5.kt
@@ -6,6 +6,17 @@ fun main() {
     //p.age = -20 // setterの例外発生
     p.age = 20
     println(p.isAdult)
+    println("--------------------------")
+    val p2 = Person2("山田太郎", 18)
+    p2.show()
+    println("--------------------------")
+    val p4 = Person4()
+    val p44 = Person4(name="名無し")
+    val p444 = Person4(age=35)
+    p4.show()
+    p44.show()
+    p444.show()
+    println("--------------------------")
 }
 
 /**
@@ -29,6 +40,44 @@ class Person() {
     /**
      * クラスの表示
      */
+    fun show() {
+        println("私は${name}です。${age}歳です。")
+    }
+}
+
+/**
+ * コンストラクタの確認用クラス
+ */
+class Person2 constructor(name:String, age:Int) {
+    // プロパティ定義
+    var name:String
+    var age:Int
+    // initialization
+    init {
+        this.name = name
+        this.age = age
+    }
+
+    fun show() {
+        println("私は${this.name}です。${this.age}歳です。")
+    }
+}
+
+/**
+ * 初期化コードの簡易化＋セカンダリコンストラクタ
+ */
+class Person3(var name:String, var age:Int) {
+    // セカンダリコンストラクタ
+    constructor(name:String):this(name,16){}
+    fun show() {
+        println("私は${name}です。${age}歳です。")
+    }
+}
+
+/**
+ * 引数の既定値ありのコンストラクタ
+ */
+class Person4(var name:String = "山田太郎", var age:Int = 10) {
     fun show() {
         println("私は${name}です。${age}歳です。")
     }
